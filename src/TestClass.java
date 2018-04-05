ca9cb5e47dc280aab776cf8cdd5d2f2b5b5725ab
Daniel (Chuan) Sha <chuanhui.sha@stonybrook.edu>
2018-04-05 19:28:29 -0400

    Added solution to leetcode problem 370

--- src/TestClass.java
+++ src/TestClass.java
@@ -613,7 +613,69 @@ public boolean canConstruct2(String ransomNote, String magazine) {
     	else {return false;}
     }
     return true;
+    //60 ms.
    }
+
+public int[] getModifiedArray(int length, int[][] updates) {
+	/*Assume you have an array of length n initialized with all 0's and are given k update operations.
+
+Each operation is represented as a triplet: [startIndex, endIndex, inc] which increments each element of 
+subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
+
+Return the modified array after all k operations were executed.
+
+Example:
+
+Given:
+
+    length = 5,
+    updates = [
+        [1,  3,  2],
+        [2,  4,  3],
+        [0,  2, -2]
+    ]
+
+Output:
+
+    [-2, 0, 3, 5, 3]
+
+Explanation:
+
+Initial state:
+[ 0, 0, 0, 0, 0 ]
+
+After applying operation [1, 3, 2]:
+[ 0, 2, 2, 2, 0 ]
+
+After applying operation [2, 4, 3]:
+[ 0, 2, 5, 5, 3 ]
+
+After applying operation [0, 2, -2]:
+[-2, 0, 3, 5, 3 ]
+*/
+	int[] returnArray = new int[length];
+	int numRows = updates.length;
+	//columns is 3: startIndex, endIndex, incrementAmount
+	int startIndex;
+	int endIndex;
+	int incrementAmount;
+	for(int i = 0; i < numRows; i++)
+	{
+		startIndex = updates[i][0];
+		endIndex = updates[i][1];
+		incrementAmount = updates[i][2];
+		
+		for(int k = startIndex; k <= endIndex; k++)
+		{
+			returnArray[k] += incrementAmount;
+		}
+	}
+	
+	return returnArray;
+}
+
+
+
 	public static void main(String[] args) {
 		/*int[] array1 = new int[5];
 		array1[0] = 1;
